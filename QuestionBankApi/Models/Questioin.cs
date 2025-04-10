b826aca90f201cb59bdb8f374a467960bdd11205
whatevertogo <1879483647@qq.com>
2025-04-10 18:25:43 +0800

    Add project.nuget.cache file with package dependencies and metadata

---
+++ QuestionBankApi/Models/Questioin.cs
@@ -0,0 +1,7 @@
+public class Question
+{
+    public required int Id { get; set; }
+    public required string Type { get; set; }
+    public required string Content { get; set; }
+    public required string Answer { get; set; }
+}
